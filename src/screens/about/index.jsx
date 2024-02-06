54c0560fd9d18cbbc22f29de46b6ed5527ac3033
Tshepiso Motshele <tshepisomotshele9@gmail.com>
2024-02-06 15:53:57 +0200

    working on the about page

---
+++ src/screens/about/index.jsx
@@ -0,0 +1,146 @@
+import React from "react";
+import { SocialIcons } from "../../icons/SocialIcons";
+import "./style.css";
+
+export const Index = () => {
+  return (
+    <div className="index">
+      <div className="overlap-wrapper">
+        <div className="overlap">
+          <div className="overlap-group">
+            <div className="nav-bar">
+              <div className="div">
+                <div className="rectangle" />
+                <div className="text-wrapper">Achiements</div>
+                <div className="text-wrapper-2">Contact</div>
+                <div className="text-wrapper-3">Portfolio</div>
+                <img
+                  className="vector"
+                  alt="Vector"
+                  src="https://cdn.animaapp.com/projects/65c1f19fe83795b5da57d4a2/releases/65c238b940da854aa8e96bcd/img/vector.svg"
+                />
+                <div className="text-wrapper-4">Home</div>
+                <div className="text-wrapper-5">About</div>
+                <SocialIcons className="social-icons" />
+                <img
+                  className="home-svgrepo-com"
+                  alt="Home svgrepo com"
+                  src="https://cdn.animaapp.com/projects/65c1f19fe83795b5da57d4a2/releases/65c238b940da854aa8e96bcd/img/home-1-svgrepo-com.svg"
+                />
+                <img
+                  className="about-information"
+                  alt="About information"
+                  src="https://cdn.animaapp.com/projects/65c1f19fe83795b5da57d4a2/releases/65c238b940da854aa8e96bcd/img/about-information-info-help-svgrepo-com.svg"
+                />
+                <img
+                  className="portfolio-svgrepo"
+                  alt="Portfolio svgrepo"
+                  src="https://cdn.animaapp.com/projects/65c1f19fe83795b5da57d4a2/releases/65c238b940da854aa8e96bcd/img/portfolio-svgrepo-com.svg"
+                />
+                <img
+                  className="education-svgrepo"
+                  alt="Education svgrepo"
+                  src="https://cdn.animaapp.com/projects/65c1f19fe83795b5da57d4a2/releases/65c238b940da854aa8e96bcd/img/education-svgrepo-com.svg"
+                />
+                <img
+                  className="location-place-pin"
+                  alt="Location place pin"
+                  src="https://cdn.animaapp.com/projects/65c1f19fe83795b5da57d4a2/releases/65c238b940da854aa8e96bcd/img/location-place-pin-svgrepo-com.svg"
+                />
+                <img
+                  className="img"
+                  alt="Vector"
+                  src="https://cdn.animaapp.com/projects/65c1f19fe83795b5da57d4a2/releases/65c238b940da854aa8e96bcd/img/vector-1.svg"
+                />
+                <img
+                  className="vector-2"
+                  alt="Vector"
+                  src="https://cdn.animaapp.com/projects/65c1f19fe83795b5da57d4a2/releases/65c1f95a886b7f388e40c1d9/img/vector-4.svg"
+                />
+                <p className="p">2024 | Designed and Developed By Tshepiso Motshele. | All rights reserved.</p>
+              </div>
+            </div>
+            <img
+              className="element"
+              alt="Element"
+              src="https://cdn.animaapp.com/projects/65c1f19fe83795b5da57d4a2/releases/65c1f9e1260467af8aa0bfe0/img/147166387-1@2x.png"
+            />
+            <div className="text-wrapper-6">Education</div>
+            <div className="text-wrapper-7">Experience</div>
+            <img
+              className="line"
+              alt="Line"
+              src="https://cdn.animaapp.com/projects/65c1f19fe83795b5da57d4a2/releases/65c238b940da854aa8e96bcd/img/line-1.svg"
+            />
+            <img
+              className="star"
+              alt="Star"
+              src="https://cdn.animaapp.com/projects/65c1f19fe83795b5da57d4a2/releases/65c238b940da854aa8e96bcd/img/star-1.svg"
+            />
+            <img
+              className="star-2"
+              alt="Star"
+              src="https://cdn.animaapp.com/projects/65c1f19fe83795b5da57d4a2/releases/65c238b940da854aa8e96bcd/img/star-1.svg"
+            />
+            <img
+              className="star-3"
+              alt="Star"
+              src="https://cdn.animaapp.com/projects/65c1f19fe83795b5da57d4a2/releases/65c238b940da854aa8e96bcd/img/star-1.svg"
+            />
+            <img
+              className="star-4"
+              alt="Star"
+              src="https://cdn.animaapp.com/projects/65c1f19fe83795b5da57d4a2/releases/65c238b940da854aa8e96bcd/img/star-1.svg"
+            />
+            <div className="AWS-certified-cloud">AWS- certified&nbsp;&nbsp;Cloud Practitioner</div>
+            <div className="text-wrapper-8">CompTIA a+ (1001-1002)</div>
+            <div className="text-wrapper-9">CompTIA N+</div>
+            <div className="text-wrapper-10">Matric</div>
+            <div className="ellipse" />
+            <div className="ellipse-2" />
+            <div className="text-wrapper-11">CapaCiTi Tech Accelerator</div>
+            <div className="ellipse-3" />
+            <div className="ellipse-4" />
+            <p className="a-full-stack">
+              A&nbsp;&nbsp;Full-Stack Development learnership program. I leanred to work rk with various programming
+              languages for front end and back end.
+            </p>
+            <div className="text-wrapper-12">Junior Full stack Developer</div>
+            <div className="text-wrapper-13">2024</div>
+            <div className="text-wrapper-14">2023-2024</div>
+            <div className="rectangle-2" />
+            <button className="button">
+              <div className="div-wrapper">
+                <div className="text-wrapper-15">Download Cv</div>
+              </div>
+            </button>
+            <div className="group">
+              <button className="overlap-group-wrapper">
+                <div className="overlap-group-2">
+                  <div className="text-wrapper-16">Get in touch</div>
+                </div>
+              </button>
+            </div>
+          </div>
+          <p className="hello">
+            <span className="span">He</span>
+            <span className="text-wrapper-17">llo</span>
+            <span className="span">,</span>
+          </p>
+          <p className="i-m-tshepiso">
+            <span className="text-wrapper-17">I’</span>
+            <span className="span">m</span>
+            <span className="text-wrapper-18">&nbsp;</span>
+            <span className="text-wrapper-17">T</span>
+            <span className="span">shepiso.</span>
+          </p>
+          <p className="i-m-passionate-about">
+            I&#39;m passionate about creating and developing clean, unique, elegant products. I specialize in building
+            Websites for small to medium size business. I keep my code clean, readable, modular and well refactored. I
+            enjoy working with complex user interfaces
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
