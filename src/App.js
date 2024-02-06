4214468857b181cb8e40a41dea6be481ae2b1f99
Tshepiso Motshele <tshepisomotshele9@gmail.com>
2024-02-06 15:40:50 +0200

    Implemented Home page
    - Added image with my details, increased the font to match image
    - added github icons

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css'
+import Home from './screens/home';
+import reportWebVitals from './reportWebVitals';
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Home/>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+reportWebVitals();
+
+
---
+++ src/screens/home/home.jsx
@@ -0,0 +1,72 @@
+import React from "react";
+import "./style.css";
+
+export const Home = () => {
+  return (
+    <div className="home">
+      <div className="home-wrapper">
+        <div className="home">
+          <div className="overlap-group">
+            <img
+              className="home-elements"
+              alt="Home elements"
+              src="https://cdn.animaapp.com/projects/65c1f19fe83795b5da57d4a2/releases/65c1f9e1260467af8aa0bfe0/img/home-elements.png"
+            />
+            <div className="rectangle" />
+            <div className="text-wrapper">Achiements</div>
+            <div className="div">Contact</div>
+            <div className="text-wrapper-2">Portfolio</div>
+            <img
+              className="vector"
+              alt="Vector"
+              src="https://cdn.animaapp.com/projects/65c1f19fe83795b5da57d4a2/releases/65c1f9e1260467af8aa0bfe0/img/vector.svg"
+            />
+            <div className="text-wrapper-3">Home</div>
+            <div className="text-wrapper-4">About</div>
+            <img
+              className="home-svgrepo-com"
+              alt="Home svgrepo com"
+              src="https://cdn.animaapp.com/projects/65c1f19fe83795b5da57d4a2/releases/65c1f9e1260467af8aa0bfe0/img/home-1-svgrepo-com.svg"
+            />
+            <img
+              className="about-information"
+              alt="About information"
+              src="https://cdn.animaapp.com/projects/65c1f19fe83795b5da57d4a2/releases/65c1f9e1260467af8aa0bfe0/img/about-information-info-help-svgrepo-com.svg"
+            />
+            <img
+              className="portfolio-svgrepo"
+              alt="Portfolio svgrepo"
+              src="https://cdn.animaapp.com/projects/65c1f19fe83795b5da57d4a2/releases/65c1f9e1260467af8aa0bfe0/img/portfolio-svgrepo-com.svg"
+            />
+            <img
+              className="education-svgrepo"
+              alt="Education svgrepo"
+              src="https://cdn.animaapp.com/projects/65c1f19fe83795b5da57d4a2/releases/65c1f9e1260467af8aa0bfe0/img/education-svgrepo-com.svg"
+            />
+            <img
+              className="location-place-pin"
+              alt="Location place pin"
+              src="https://cdn.animaapp.com/projects/65c1f19fe83795b5da57d4a2/releases/65c1f9e1260467af8aa0bfe0/img/location-place-pin-svgrepo-com.svg"
+            />
+            <img
+              className="element"
+              alt="Element"
+              src="https://cdn.animaapp.com/projects/65c1f19fe83795b5da57d4a2/releases/65c1f9e1260467af8aa0bfe0/img/147166387-1@2x.png"
+            />
+            <img
+              className="img"
+              alt="Vector"
+              src="https://cdn.animaapp.com/projects/65c1f19fe83795b5da57d4a2/releases/65c1f9e1260467af8aa0bfe0/img/vector-1.svg"
+            />
+            <img
+              className="vector-2"
+              alt="Vector"
+              src="https://cdn.animaapp.com/projects/65c1f19fe83795b5da57d4a2/releases/65c1f95a886b7f388e40c1d9/img/vector-4.svg"
+            />
+            <p className="p">2024 | Designed and Developed By Tshepiso Motshele. | All rights reserved.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
