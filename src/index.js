fc9ef59476fec549d7fdaf956db7068ca61e5bc4
Tshepiso Motshele <tshepisomotshele9@gmail.com>
2024-02-07 10:37:35 +0200

    fixed the style for the home page

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { Home } from './screens/home/home'
+import { Home } from './components/screens/home/home'
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
