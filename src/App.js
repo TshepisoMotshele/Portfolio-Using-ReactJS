6c92ea3781d2d1f1502d2718809bcf2ef42371b9
Tshepiso Motshele <tshepisomotshele9@gmail.com>
2024-02-07 13:42:33 +0200

    updated js

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css'
 import Home from './screens/home';
 import reportWebVitals from './reportWebVitals';
-
+import { About } from './components/screens/about';
 
 ReactDOM.render(
   <React.StrictMode>
     <Home/>
+    <About/>
   </React.StrictMode>,
   document.getElementById('root')
 );
